f34a5813055f6dca396009d8b440e6c5add03ed4
Geert Wirken <geert@gwirken.nl>
2013-11-11 00:02:42 +0100

    xmltest wat opgeruimd, ondersteunt nu UTF-8 output

--- xmltest.py
+++ xmltest.py
@@ -1,18 +1,36 @@
 import infoplus_dvs
 import glob
 
+# Maak output in utf-8 mogelijk in Python 2.x:
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+# Laad testdata:
 xmlFiles = glob.glob('./testdata/*/*.xml') + glob.glob('./testdata/treinlog/*/*.xml')
 xmlFiles = sorted(xmlFiles)
 
+counter = 0
+
+# Doorloop alle testbestanden:
 for xmlFile in xmlFiles:
+	# Laad bestand in:
 	f = open(xmlFile, 'r')
-	string = ""
-	while 1:
+	content = ""
+
+	while True:
 		line = f.readline()
-		if not line:break
-		string += line
+		if not line:
+			break
+
+		content += line
 
 	f.close()
 
-	trein = infoplus_dvs.parse_trein(string)
+	# Parse treinbericht:
+	trein = infoplus_dvs.parse_trein(content)
 	print trein
+
+	counter = counter + 1
+
+print "%s XML files geparsed" % counter
