f11e052e2ce609c4e83d396a39feb9f608c51d49
Geert Wirken <geert@gwirken.nl>
2013-11-12 01:11:35 +0100

    Eerste aanzet voor in-memory datastore per station en per trein.

--- dvs-daemon.py
+++ dvs-daemon.py
@@ -4,7 +4,8 @@ from gzip import GzipFile
 from cStringIO import StringIO
 from datetime import datetime
 
-import traceback
+import pprint
+
 import infoplus_dvs
 
 # Maak output in utf-8 mogelijk in Python 2.x:
@@ -18,6 +19,7 @@ socket = context.socket(zmq.SUB)
 
 print "Collecting updates from DVS server..."
 socket.connect ("tcp://post.ndovloket.nl:7660")
+#socket.connect ("tcp://46.19.34.170:8100")
 socket.setsockopt(zmq.SUBSCRIBE, '')
 
 poller = zmq.Poller()
@@ -26,40 +28,76 @@ poller.register(socket, zmq.POLLIN)
 starttime = datetime.now()
 msgNumber = 0
 
+# Datastores:
+stationStore = { }
+treinStore = { }
+
+
 #socks = dict(poller.poll())
 #print socks
 
-errorfile = open('dvs-error.log', 'w')
+try:
+	while True:
+		multipart = socket.recv_multipart()
+		address = multipart[0]
+		content = GzipFile('','r',0,StringIO(''.join(multipart[1:]))).read()
 
-while True:
-	multipart = socket.recv_multipart()
-	address = multipart[0]
-	content = GzipFile('','r',0,StringIO(''.join(multipart[1:]))).read()
+		# Parse trein xml:
+		try:
+			trein = infoplus_dvs.parse_trein(content)
+			#print datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "## %9s ## trst=%s " % (msgNumber, trein.status), trein
 
-	# Parse trein xml:
-	try:
-		trein = infoplus_dvs.parse_trein(content)
-		print datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "## %9s ## " % msgNumber, trein
+			ritStationCode = trein.ritStation.code
+			
+			# Maak item in stationStore indien niet aanwezig:
+			if ritStationCode not in stationStore:
+				stationStore[ritStationCode] = {}
 
-		if trein.ritStation.code in ['RTD', 'UT', 'ASDZ', 'ASD'] or trein.treinNr in ['1927', '1929', '1941', '1743', '36790', '21459', '1409', '21463', '1413', '5710', '919', '9315', '9322', '1218']:
-			# Save event:
-			filename = '/home/geert/ndovtest/treinlog/%s-%s_%s.xml' % (trein.treinNr, trein.ritStation.code, datetime.now().strftime("%Y%m%d-%H%M%S"))
-			logfile = open(filename, 'w')
-			logfile.write(content)
-			logfile.close()
-	except Exception as e:
-		print "!!!!!! ERROR! !!!!! %s" % e
+			# Maak item in treinStore indien niet aanwezig
+			if trein.treinNr not in treinStore:
+				treinStore[trein.treinNr] = {}
+			
+			if trein.status == '5':
+				# Trein vertrokken
+				print datetime.now().strftime("%H:%M:%S"), ' -->> trein %6s vertrokken van  %s' % (trein.treinNr, ritStationCode)
 
-		exc_type, exc_value, exc_traceback = sys.exc_info()
-		print "*** print_tb:"
-		traceback.print_tb(exc_traceback, limit=1)
-		traceback.print_tb(exc_traceback, limit=1, file=errorfile)
-		print "*** print_exception:"
-		traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=errorfile)
-		
-	msgNumber = msgNumber + 1
+				# Verwijder uit stationStore
+				if trein.treinNr in stationStore[ritStationCode]:
+					del(stationStore[ritStationCode][trein.treinNr])
 
-errorfile.close()
+				# Verwijder uit treinStore
+				if ritStationCode in treinStore[trein.treinNr]:
+					del(treinStore[trein.treinNr][ritStationCode])
+					if len(treinStore[trein.treinNr]) == 0:
+						del(treinStore[trein.treinNr])
+			else:
+				# Update of insert trein aan station:
+				stationStore[ritStationCode][trein.treinNr] = trein
+				treinStore[trein.treinNr][ritStationCode] = trein
 
-socket.close()
-context.term()
+				if trein.status == '2':
+					print datetime.now().strftime("%H:%M:%S"), ' >>-- trein %6s aangekomen te   %s' % (trein.treinNr, ritStationCode)
+				if trein.status == '0':
+					print datetime.now().strftime("%H:%M:%S"), ' ---- trein %6s status onbekend %s' % (trein.treinNr, ritStationCode)
+
+		except Exception as e:
+			print "!!!!!! Error: %s" % e
+			
+		msgNumber = msgNumber + 1
+
+		# Statistics update every 100 messages:
+		if msgNumber % 100 == 0:
+			print "** (i) Statistieken **"
+			print "   Station store: %s stations" % len(stationStore)
+			print "   Trein store: %s treinen" % len(treinStore)
+			print "   Messages verwerkt: %s" % msgnum
+except KeyboardInterrupt:
+	print "Exiting..."
+
+	socket.close()
+	context.term()
+
+	print "Station store:"
+	pprint.pprint(stationStore)
+
+	print "Processed %s messages" % msgNumber
