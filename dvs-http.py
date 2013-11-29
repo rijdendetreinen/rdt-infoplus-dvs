8c653154abe9def9d52ec20d6fa35b674c8f9fdf
Geert Wirken <g.wirken@interexperts.nl>
2013-11-29 11:55:45 +0100

    In dvs-http switch tussen lokaal en productie.

--- dvs-http.py
+++ dvs-http.py
@@ -1,8 +1,32 @@
-import bottle
-import dvs_http_interface
+#!/usr/bin/env python
 
-# Default config (tijdelijk)
-dvs_http_interface.dvs_client_server = "tcp://46.19.34.170:8120"
+"""
+Test tool om een lokale HTTP server te starten.
+"""
+
+import bottle
+import argparse
+import dvs_http_interface
+import logging
+
+# Initialiseer argparse
+parser = argparse.ArgumentParser(description='DVS HTTP interface test tool')
+
+parser.add_argument('-l', '--lokaal', dest='lokaal',
+    action='store_true', help='Test met lokale server 127.0.0.1:8120')
+
+args = parser.parse_args()
+
+if args.lokaal == True:
+    dvs_http_interface.dvs_client_server = "tcp://127.0.0.1:8120"
+else:
+    dvs_http_interface.dvs_client_server = "tcp://46.19.34.170:8120"
+
+# Stel logger in:
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+logger.info("Server: %s", dvs_http_interface.dvs_client_server)
 
 bottle.debug(True)
 bottle.run(host='localhost', port=8080, reloader=True)
