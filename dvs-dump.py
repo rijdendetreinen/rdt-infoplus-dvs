53818f66b46737cc4559f7153bca82e8c5a809b0
Geert Wirken <g.wirken@interexperts.nl>
2013-12-08 11:43:27 +0100

    DVS dump tool toegevoegd

---
+++ dvs-dump.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+"""
+Debug script welke rechtstreeks opdrachten naar DVS stuurt.
+"""
+
+import zmq
+import argparse
+import pprint
+
+def main():
+	"""
+	Main functie
+	"""
+
+	# Initialiseer argparse
+	parser = argparse.ArgumentParser(description='DVS test tool. Stuur opdracht naar DVS daemon')
+
+	parser.add_argument('-l', '--lokaal', dest='lokaal',
+	    action='store_true', help='Test met lokale server 127.0.0.1:8120')
+	parser.add_argument('OPDRACHT', nargs='?',
+	    action='store', help='opdracht naar DVS server', default='store/trein')
+
+	args = parser.parse_args()
+
+	if args.lokaal == True:
+	    dvs_client_server = "tcp://127.0.0.1:8120"
+	else:
+	    dvs_client_server = "tcp://46.19.34.170:8120"
+
+	opdracht = args.OPDRACHT
+
+	print "Opdracht naar DVS: %s" % opdracht
+	print "--------------------------------"
+	print
+
+	# Maak verbinding
+	context = zmq.Context()
+	client = context.socket(zmq.REQ)
+	client.connect(dvs_client_server)
+
+	# Stuur opdracht:
+	client.send(opdracht)
+	treinen = client.recv_pyobj()
+
+	pretty = pprint.PrettyPrinter(indent=4)
+
+	pretty.pprint(treinen)
+
+if __name__ == "__main__":
+    main()
