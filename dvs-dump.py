90a8bd2b8e56ee7ff6cf281f799d5d5ea24ac17b
Geert Wirken <g.wirken@interexperts.nl>
2013-12-08 16:28:56 +0100

    DVS dump tool tabs -> spaties

--- dvs-dump.py
+++ dvs-dump.py
@@ -10,47 +10,48 @@ import pprint
 import sys
 
 def main():
-	"""
-	Main functie
-	"""
+    """
+    Main functie
+    """
 
-	# Maak output in utf-8 mogelijk in Python 2.x:
-	reload(sys)
-	sys.setdefaultencoding("utf-8")
+    # Maak output in utf-8 mogelijk in Python 2.x:
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
 
-	# Initialiseer argparse
-	parser = argparse.ArgumentParser(description='DVS test tool. Stuur opdracht naar DVS daemon')
+    # Initialiseer argparse
+    parser = argparse.ArgumentParser(
+        description='DVS test tool. Stuur opdracht naar DVS daemon')
 
-	parser.add_argument('-l', '--lokaal', dest='lokaal',
-	    action='store_true', help='Test met lokale server 127.0.0.1:8120')
-	parser.add_argument('OPDRACHT', nargs='?',
-	    action='store', help='opdracht naar DVS server', default='store/trein')
+    parser.add_argument('-l', '--lokaal', dest='lokaal',
+        action='store_true', help='Test met lokale server 127.0.0.1:8120')
+    parser.add_argument('OPDRACHT', nargs='?',
+        action='store', help='opdracht naar DVS server', default='store/trein')
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	if args.lokaal == True:
-	    dvs_client_server = "tcp://127.0.0.1:8120"
-	else:
-	    dvs_client_server = "tcp://46.19.34.170:8120"
+    if args.lokaal == True:
+        dvs_client_server = "tcp://127.0.0.1:8120"
+    else:
+        dvs_client_server = "tcp://46.19.34.170:8120"
 
-	opdracht = args.OPDRACHT
+    opdracht = args.OPDRACHT
 
-	print "Opdracht naar DVS: %s" % opdracht
-	print "--------------------------------"
-	print
+    print "Opdracht naar DVS: %s" % opdracht
+    print "--------------------------------"
+    print
 
-	# Maak verbinding
-	context = zmq.Context()
-	client = context.socket(zmq.REQ)
-	client.connect(dvs_client_server)
+    # Maak verbinding
+    context = zmq.Context()
+    client = context.socket(zmq.REQ)
+    client.connect(dvs_client_server)
 
-	# Stuur opdracht:
-	client.send(opdracht)
-	treinen = client.recv_pyobj()
+    # Stuur opdracht:
+    client.send(opdracht)
+    treinen = client.recv_pyobj()
 
-	pretty = pprint.PrettyPrinter(indent=4)
+    pretty = pprint.PrettyPrinter(indent=4)
 
-	pretty.pprint(treinen)
+    pretty.pprint(treinen)
 
 if __name__ == "__main__":
     main()
