c9d02a857e30f3fa437f1b6fc48d1c9070fb458e
Geert Wirken <geert@gwirken.nl>
2014-04-21 17:36:57 +0200

    Nagios test scriptje toegevoegd

---
+++ contrib/nagios/dvs_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os, sys
+system_status = os.popen("/opt/rdt/infoplus-dvs/dvs-dump.py status/status -q").readline().strip()
+
+print system_status
+
+if system_status == "'OK'":
+    print "OK - No downtime detected"
+    sys.exit(0)
+elif system_status == "'RECOVERING'":
+    print "WARNING - Recovering from downtime"
+    sys.exit(1)
+elif system_status == "'DOWN'":
+    print "CRITICAL - Downtime detected, not recovering"
+    sys.exit(2)
+else:
+    print "UKNOWN"
+    sys.exit(3)
